acc5ca654d4bc3500aa4d5f8768afcf7d549b316
unknown <mhedouin@ami2.com>
2023-01-11 12:32:33 +0100

    tuto pizza legend ep 4

--- Overworld.js
+++ Overworld.js
@@ -1,34 +1,49 @@
 class Overworld {
-    constructor(config) {
-        this.element = config.element;
-        this.canvas = this.element.querySelector(".game-canvas");
-        this.ctx = this.canvas.getContext("2d");
-    }
+ constructor(config) {
+   this.element = config.element;
+   this.canvas = this.element.querySelector(".game-canvas");
+   this.ctx = this.canvas.getContext("2d");
+   this.map = null;
+ }
 
-    init() {
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0);
-        };
-        image.src = "images/maps/DemoLower.png";
+ startGameLoop() {
+   const step = () => {
 
-        // Place some game objects
+    //Clear off the canvas
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-        })
+    //Draw Lower layer
+    this.map.drawLowerImage(this.ctx);
 
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "images/characters/people/npc1.png",
-        })
+    //Draw Game Objects
+    Object.values(this.map.gameObjects).forEach(object => {
+      object.update({
+        arrow: this.directionInput.direction,
+      })
+      object.sprite.draw(this.ctx);
+    })
 
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx); 
-        }, 200)
+    //Draw Upper layer
+    this.map.drawUpperImage(this.ctx);
 
-    }
+
+     requestAnimationFrame(() => {
+      step();   
+     })
+   }
+   step();
+ }
+
+ init() {
+  this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+  
+  this.directionInput = new DirectionInput();
+  this.directionInput.init();
+  
+  this.startGameLoop();
+  
+
+
+ }
+
 }
